ad582ac358f37e439620d3f0c250f679fab1127f
Gabriel Espinosa <gabiespinosa138@gmail.com>
2025-12-10 16:47:35 -0800

    feat: created a basic express server for my web dev website contact from.

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+
+const server = express();
+
+server.use(express.json()); // parse JSON body
+
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
