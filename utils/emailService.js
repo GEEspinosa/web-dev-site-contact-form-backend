e7ea93142a80ad06ecabd5523c697ba0d5f51589
Gabriel Espinosa <gabiespinosa138@gmail.com>
2025-12-12 10:48:57 -0800

    fix: new key, gitignore works

--- utils/emailService.js
+++ utils/emailService.js
@@ -1,31 +1,3 @@
-// const nodemailer = require("nodemailer");
-
-// async function sendMail({ name, email, subject, text }) {
-//   const transporter = nodemailer.createTransport({
-//     host: "smtp.gmail.com",
-//     port: 465,
-//     secure: true,
-//     auth: { user: process.env.MAIL_USER, pass: process.env.MAIL_PASS },
-//   });
-
-//   const mailOptions = {
-//     from: email,
-//     to: process.env.MAIL_USER,
-//     subject: `Contact form: ${subject}`,
-//     text: `
-//         Name: ${name}
-//         Email: ${email}
-//         Message:
-//         ${text}
-//         `,
-//   };
-
-//   return transporter.sendMail(mailOptions);
-// }
-
-// module.exports = sendMail;
-
-
 const { Resend } = require("resend");
 
 const resend = new Resend(process.env.RESEND_API_KEY);
