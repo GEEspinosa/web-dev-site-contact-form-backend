81f372256a6cd65ff7bb8af37b98c6f9190ffa94
Gabriel Espinosa <gabiespinosa138@gmail.com>
2025-12-12 00:44:50 -0800

    fix: adding resend and bypassing Gmail password key.

--- utils/emailService.js
+++ utils/emailService.js
@@ -1,26 +1,49 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
+
+// async function sendMail({ name, email, subject, text }) {
+//   const transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 465,
+//     secure: true,
+//     auth: { user: process.env.MAIL_USER, pass: process.env.MAIL_PASS },
+//   });
+
+//   const mailOptions = {
+//     from: email,
+//     to: process.env.MAIL_USER,
+//     subject: `Contact form: ${subject}`,
+//     text: `
+//         Name: ${name}
+//         Email: ${email}
+//         Message:
+//         ${text}
+//         `,
+//   };
+
+//   return transporter.sendMail(mailOptions);
+// }
+
+// module.exports = sendMail;
+
+
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendMail({ name, email, subject, text }) {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: { user: process.env.MAIL_USER, pass: process.env.MAIL_PASS },
-  });
-
-  const mailOptions = {
-    from: email,
-    to: process.env.MAIL_USER,
+  return await resend.emails.send({
+    from: "Portfolio Contact Form <onboarding@resend.dev>", // Safe default
+    to: "gabiespinosa138@gmail.com", // Your inbox
     subject: `Contact form: ${subject}`,
     text: `
-        Name: ${name}
-        Email: ${email}
-        Message:
-        ${text}
-        `,
-  };
+Name: ${name}
+Email: ${email}
 
-  return transporter.sendMail(mailOptions);
+Message:
+${text}
+    `,
+  });
 }
 
 module.exports = sendMail;
+
